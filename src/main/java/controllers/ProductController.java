8f959607f71a65445bcd15b22dd1af1b6dac2760
nanonachino-522 <60164853+nanonachino-522@users.noreply.github.com>
2021-05-08 14:56:10 +0700

    edit_controller_Post_Delete_Put

--- src/main/java/controllers/ProductController.java
+++ src/main/java/controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import repositories.repoProduct;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
 import java.util.List;
 
 //@CrossOrigin(origins = "http://localhost:8080")
@@ -26,19 +27,34 @@ public class ProductController {
     @PostMapping("/add")
     public Product add(@RequestBody Product newproduct){
         if(repoPro.findById(newproduct.getIdPro()).orElse(null) != null && repoPro.findByName(newproduct.getProName()) != null){
-
+            throw new ProductException(ExceptionRepo.ERROR_CODE.PRODUCT_ALREADY_EXIST,"Id : "+newproduct.getIdPro() + " AND Name : "+newproduct.getProName() + " Have Already");
+        }else if(repoPro.findById(newproduct.getIdPro()).orElse(null) != null){
+            throw new ProductException(ExceptionRepo.ERROR_CODE.PRODUCT_ID_ALREADY_EXIST,"Id : "+newproduct.getIdPro() + " Have Already");
+        }else if(repoPro.findByName(newproduct.getProName()) != null){
+            throw new ProductException(ExceptionRepo.ERROR_CODE.PRODUCT_NAME_ALREADY_EXIST,"Name : "+newproduct.getProName() + " Have Already");
         }
-        return null;
+        return repoPro.save(newproduct);
     }
 
     @DeleteMapping("/delete/{idPro}")
-    public List<Product> delete(){
-        return null;
+    public void delete(@PathVariable Long idPro){
+        Product product = repoPro.findById(idPro).orElse(null);
+        if(product == null){
+            throw new ProductException(ExceptionRepo.ERROR_CODE.PRODUCT_DOES_NOT_EXIST,"Can't delete. Id : "+idPro + " does not exist.");
+        }
+        repoPro.deleteById(idPro);
     }
 
     @PutMapping("/edit/{idPro}")
-    public Product edit(){
-        return null;
+    public Product edit(@RequestBody Product editProduct){
+        Product productId = repoPro.findById(editProduct.getIdPro()).orElse(null);
+        Product productName = repoPro.findByName(editProduct.getProName());
+        if(productId == null){
+            throw new ProductException(ExceptionRepo.ERROR_CODE.PRODUCT_DOES_NOT_EXIST,"Can't edit. Id : "+editProduct.getIdPro() + " does not exist.");
+        }else if(productName != null && productId.getIdPro() != productName.getIdPro()){
+            throw new ProductException(ExceptionRepo.ERROR_CODE.PRODUCT_NAME_ALREADY_EXIST,"Can't edit . Name : "+editProduct.getProName() + " already exist.");
+        }
+        return repoPro.save(editProduct);
     }
 
 }
--- src/test/java/sit/integrated/project/IntegratedApplicationTests.java
+++ src/test/java/sit/integrated/project/IntegratedApplicationTests.java
@@ -1,9 +1,12 @@
 package sit.integrated.project;
 
+import org.hibernate.type.descriptor.sql.SmallIntTypeDescriptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+
 class IntegratedApplicationTests {
 
     @Test
